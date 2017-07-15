e8f3a3b0ca292ad96111a41aa030b540097dd975
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-15 21:24:50 +0700

    ket thuc

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => cb(null, './public'),
+    filename: (req, file, cb) => cb(null, Date.now() + '.png')
+});
+
+const upload = multer({ storage }).single('avatar');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -6,3 +14,9 @@ app.set('views', './views');
 app.use(express.static('./public'));
 
 app.get('/upload', (req, res) => res.render('upload'));
+
+app.post('/upload', upload, (req, res) => {
+    res.send('Da nhan file ' + req.file.originalname);
+});
+
+app.listen(3000, () => console.log('Server is running'));
